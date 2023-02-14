3d0b410472b177633f2fefb531b88f46d86b4dbd
Manu <88348637+manuziny@users.noreply.github.com>
2023-02-14 14:25:20 -0300

    Add files via upload

---
+++ fparac.py
@@ -0,0 +1,9 @@
+fahrenheit = float(input('Digite a temperatura em F: '))
+celsius = 5 * ((fahrenheit-32)/9)
+
+print('A temperatura em Celsius é: {} C'.format(celsius))
+
+celsiusUsu = float(input('Digite a temperatura em Celsius: '))
+converFah = celsiusUsu * 1.8 + 32
+
+print('A temperatura em F é: {} F'.format(converFah))
